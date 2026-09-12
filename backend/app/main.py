708630397b1e5def1475b0cb37588e6156a1f9bf
jayo14 <ayobamidayo2007@gmail.com>
2026-09-12 23:58:52 +0100

    Allow all CORS methods and headers in FastAPI middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,8 +28,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.cors_origin_list,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allow_headers=["Authorization", "Content-Type", "Accept"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
